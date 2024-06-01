157701f5b098a6ce70554e15ab74f7fa1e7258dd
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-01 21:12:44 +0700

    js lab 86

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+// function extractCurrencyValue(string, rate) {
+//   let dollar = parseFloat(string.replace("$", ""));
+//   let bath = dollar * rate;
+//   return bath;
+// }
+
+
+ extractCurrencyValue = (string, rate) => {
+  let dollar = parseFloat(string.replace("$", ""));
+  let bath = dollar * rate;
+  return bath;
+}
+console.log(extractCurrencyValue("$120", 30));
+console.log(extractCurrencyValue("$120", 36));
